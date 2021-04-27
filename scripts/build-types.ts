9001780db2e24daf08f0b7e8bad639b2102d442c
Micah Halter <micah@balena.io>
2021-04-27 09:32:16 -0400

    Cleaned up buildtypes script
    Signed-off-by: Micah Halter <micah@balena.io>

--- scripts/build-types.ts
+++ scripts/build-types.ts
@@ -1,24 +1,21 @@
 import cue from 'cuelang-js';
-import { default as openapiTS } from 'openapi-typescript';
+import openapiTS from 'openapi-typescript';
 
 import { promises as fs } from 'fs';
 
 const cueToTSTypes = async (input: string) => {
-	const cueresult = await cue('export', [input], { '--out': 'openapi' });
-	if (cueresult.code !== 0) {
-		console.error(cueresult.stderr);
-		throw new Error('Error exporting cue as openapi');
+	const exportResults = await cue('export', [input], { '--out': 'openapi' });
+	if (exportResults.code !== 0) {
+		throw new Error(`Failed to export cue to openapi: ${exportResults.stderr}`);
 	}
-	const openapi = JSON.parse(cueresult.stdout);
+	const openapi = JSON.parse(exportResults.stdout);
 	return openapiTS(openapi);
 };
 
-(async () => {
-	cueToTSTypes('types/definitions.cue')
-		.catch((err) => console.error(err))
-		.then((ts: string) => {
-			const output = "types/cuetypes.ts";
-			fs.writeFile(output, ts)
-			console.log(`Built types to ${output}`);
-		});
-})();
+cueToTSTypes('types/definitions.cue')
+	.catch((err) => console.error(err))
+	.then((ts: string) => {
+		const output = "types/cuetypes.ts";
+		fs.writeFile(output, ts)
+		console.log(`Built types to ${output}`);
+	});
